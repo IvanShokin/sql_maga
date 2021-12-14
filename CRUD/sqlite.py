e228c4263aa06620a373be3afb723a4d3c53c7d6
Aleksey <Parputs.a.m.four@yandex.ru>
2021-12-14 21:30:06 +0300

    init

--- CRUD/sqlite.py
+++ CRUD/sqlite.py
@@ -1,5 +1,6 @@
 
 
+
 def createUser(cur, data):
     cur.execute("INSERT INTO users(f_name, l_name, gender, age) VALUES(?, ?, ?, ?, ?);", data)
     return print(f'user is created')
@@ -11,14 +12,30 @@ def createProject(cur, data):
 
 
 def getListUsers(cur):
-    all_users =  cur.execute("SELECT user FROM users").fetchall()
-    return all_users
+    return cur.execute("SELECT user FROM users").fetchall()
 
 
 def getListProjects(cur):
-    all_projects =  cur.execute("SELECT title FROM projects").fetchall()
-    return all_projects
+      return cur.execute("SELECT title FROM projects").fetchall()
 
 
 def getUserInfo(cur, user):
-    pass
+    return cur.execute("SELECT * FROM users WHERE f_name = user").fetch()
+
+
+
+def getProjectInfo(cur, project):
+    return cur.execute("SELECT * FROM projects WHERE title = project").fetch()
+
+
+
+def getUserId(cur, user):
+	return cur.execute("SELECT id FROM users WHERE f_name = user").fetch()
+
+
+def getProjectId(cur, project):
+	return cur.execute("SELECT id FROM projects WHERE title = project").fetch()
+
+
+def getUserProjects(cur, user):
+	pass
