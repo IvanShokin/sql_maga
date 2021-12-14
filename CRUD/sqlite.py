891e33b51b1b853df220b4d153e15762439f9798
Nerzhul <sinokbati@gmail.com>
2021-12-14 19:05:20 +0300

    add command

--- CRUD/sqlite.py
+++ CRUD/sqlite.py
@@ -1,17 +1,20 @@
 import sqlite3
 
 
-def create():
+def create(cur, entity, data):
+    if entity == 'user':
+        cur.execute("INSERT INTO users(user_id, f_name, l_name, gender) VALUES(?, ?, ?, ?);", data)
+    elif entity == 'project':
+        cur.execute("INSERT INTO project(order_id, title, description, access_level) VALUES(?, ?, ?, ?);", data)
+
+
+def update(cur):
     pass
 
 
-def update():
+def read(cur):
     pass
 
 
-def read():
-    pass
-
-
-def delete():
+def delete(cur):
     pass
--- security_company.py
+++ security_company.py
@@ -1,5 +1,49 @@
 from CRUD import sqlite
+import sqlite3
+
+# NULL — значение NULL
+# INTEGER — целое число
+# REAL — число с плавающей точкой
+# TEXT — текст
+# BLOB — бинарное представление крупных объектов, хранящееся в точности с тем, как его ввели
 
 
+conn = sqlite3.connect('staff.db')
+cur = conn.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS users(
+   user_id INT PRIMARY KEY,
+   f_name TEXT,
+   l_name TEXT,
+   gender TEXT);
+""")
+
+cur.execute("""CREATE TABLE IF NOT EXISTS project(
+   order_id INT PRIMARY KEY,
+   title TEXT,
+   description TEXT,
+   access_level INTEGER);
+""")
 
 
+command = int(input('''
+        1 - Добавить сотрудника
+        2 - Создать проект
+        3 - Поулчить список сотрудников
+        4 - Получить список проектов
+'''))
+
+if command == 1:
+    pass
+
+elif command == 2:
+    pass
+
+elif command == 3:
+    cur.execute("SELECT * FROM users;")
+    three_results = cur.fetchmany(3)
+    print('\n'.join(three_results))
+elif command == 4:
+    pass
+
+conn.commit()
