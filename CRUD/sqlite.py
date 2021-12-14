48bdaf155b545eef6cae13013a9acf80ad42c609
Aleksey <Parputs.a.m.four@yandex.ru>
2021-12-14 21:41:17 +0300

    new com

--- CRUD/sqlite.py
+++ CRUD/sqlite.py
@@ -2,41 +2,35 @@
 
 def createUser(cur, data):
     cur.execute("INSERT INTO users(f_name, l_name, gender, age) VALUES(?, ?, ?, ?, ?);", data)
-    return print(f'user is created')
 
 
 def createProject(cur, data):
     cur.execute("INSERT INTO project(title, description, access_level) VALUES(?, ?, ?, ?);", data)
-    return print(f'user is created')
 
 
 def getListUsers(cur):
     return cur.execute("SELECT user FROM users").fetchall()
 
 
-
 def getListProjects(cur):
-    return cur.execute("SELECT title FROM projects").fetchall()
-
+    return cur.execute("SELECT title FROM project").fetchall()
 
 
 def getUserInfo(cur, user):
     return cur.execute("SELECT * FROM users WHERE f_name = user").fetch()
 
 
-
 def getProjectInfo(cur, project):
-    return cur.execute("SELECT * FROM projects WHERE title = project").fetch()
-
+    return cur.execute("SELECT * FROM project WHERE title = project").fetch()
 
 
 def getUserId(cur, user):
 	return cur.execute("SELECT id FROM users WHERE f_name = user").fetch()
 
 
-def getProjectId(cur, project):
-	return cur.execute("SELECT id FROM projects WHERE title = project").fetch()
-
-
 def getUserProjects(cur, user):
-	pass
+	return cur.execute("SELECT project_id FROM user_project WHERE title = project").fetch()
+
+
+def getProjectId(cur, project):
+	return cur.execute("SELECT id FROM project WHERE title = project").fetch()
