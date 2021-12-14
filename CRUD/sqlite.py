f3be6f4dfce2a7bcab97c7538024c79156e5e618
Aleksey <Parputs.a.m.four@yandex.ru>
2021-12-14 22:44:45 +0300

    new com

--- CRUD/sqlite.py
+++ CRUD/sqlite.py
@@ -15,26 +15,26 @@ def getListProjects(cur):
 
 
 def getUserInfo(cur, user):
-    return cur.execute(f"SELECT * FROM users WHERE f_name = {user}").fetch()
+    return cur.execute(f"SELECT * FROM users WHERE f_name = {user}").fetchone()
 
 
 def getProjectInfo(cur, project):
-    return cur.execute(f"SELECT * FROM project WHERE title = {project}").fetch()
+    return cur.execute(f"SELECT * FROM project WHERE title = {project}").fetchone()
 
 
 def getUserId(cur, user):
-    return cur.execute(f"SELECT id FROM users WHERE f_name = {user}").fetch()
+    return cur.execute(f"SELECT id FROM users WHERE f_name = {user}").fetchone()
 
 
 def getUserProjects(cur, user):
-    user_id = getUserId(cur, user)
-    return cur.execute(f"SELECT project_id FROM user_project WHERE user_id = {user_id}").fetchAll()
+    param = getUserId(cur, user)
+    return cur.execute(f"SELECT project_id FROM user_project WHERE user_id = {param}").fetchAll()
 
 
 def getProjectId(cur, project):
-    return cur.execute(f"SELECT id FROM project WHERE title = {project}").fetch()
+    return cur.execute(f"SELECT id FROM project WHERE title = {project}").fetchone()
 
 
 def getProjectUsers(cur, project):
-    project_id = getProjectId(cur, project)
-    return cur.execute(f"SELECT user_id FROM user_project WHERE project_id = {project_id}").fetchAll()
+    param = getProjectId(cur, project)
+    return cur.execute(f"SELECT user_id FROM user_project WHERE project_id = {param}").fetchAll()
